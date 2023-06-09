966dcfdf561f48a78150f9a8efda62a099defd8f
Andrick Takisawa <andricktakisawa@MacBook-Pro-de-Andrick.local>
2023-06-08 19:09:16 -0600

    Commit

--- admin/home.php
+++ admin/home.php
@@ -36,37 +36,42 @@ if ($email != false && $password != false) {
 
 <head>
     <meta charset="UTF-8">
-    <title><?php echo $fetch_info['name'] ?> | Home</title>
+    <title>Anomaly | Panel</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <style>
         @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
 
-        nav {
-            padding-left: 100px !important;
-            padding-right: 100px !important;
-            background: #e50000;
+        body {
             font-family: 'Poppins', sans-serif;
+            background-color: #f5f5f5;
+        }
+
+        nav {
+            background: rgba(255, 255, 255, 0.8);
+            backdrop-filter: blur(10px);
+            color: #000;
+            padding: 10px 30px;
         }
 
         nav a.navbar-brand {
-            color: #fff;
-            font-size: 20px !important;
+            color: #000;
+            font-size: 20px;
             font-weight: 500;
         }
 
-        button a {
-            color: #e50000;
-            font-weight: 500;
-        }
-
-        button a:hover {
-            text-decoration: none;
+        .container {
+            padding: 20px;
+            margin-top: 20px;
         }
 
         .data-table {
             width: 100%;
             border-collapse: collapse;
             margin: 20px 0;
+            border-radius: 10px;
+            overflow: hidden;
+            box-shadow: 0 0 10px rgb(255 7 7 / 10%)
         }
 
         .data-table-header {
@@ -77,7 +82,7 @@ if ($email != false && $password != false) {
         }
 
         .data-table-row:nth-child(even) {
-            background: #f2f2f2;
+            background: rgba(255, 255, 255, 0.5);
         }
 
         .data-table-cell {
@@ -93,75 +98,148 @@ if ($email != false && $password != false) {
         }
 
         .footer {
-            background: #e50000;
-            color: #fff;
-            position: fixed;
-            left: 0;
-            bottom: 0;
-            width: 100%;
+            background: rgba(255, 255, 255, 0.8);
+            backdrop-filter: blur(10px);
+            color: #000;
             padding: 20px 0;
             text-align: center;
+            margin-top: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
         }
 
         .footer a {
-            color: #eee;
+            color: #000;
             font-weight: 700;
             text-decoration: none;
+        }
+
+        .copy-icon {
+            cursor: pointer;
+            color: #fb3a3a;
+            transition: color 0.3s ease;
+        }
+
+        .copy-icon:hover {
+            color: #e50000;
+        }
+
+        .glassmorphism {
+            background: rgba(255, 255, 255, 0.5);
+            backdrop-filter: blur(20px);
+            border-radius: 10px;
+            padding: 20px;
+            margin-bottom: 20px;
+            border: 1px solid rgba(255, 255, 255, 0.2);
         }
     </style>
 </head>
 
 <body>
+    <?php require_once "controllerUserData.php"; ?>
+    <?php
+    $email = $_SESSION['email'];
+    $password = $_SESSION['password'];
+    if ($email != false && $password != false) {
+        $sql = "SELECT * FROM usertable WHERE email = '$email'";
+        $run_Sql = mysqli_query($con, $sql);
+        if ($run_Sql) {
+            $fetch_info = mysqli_fetch_assoc($run_Sql);
+            $status = $fetch_info['status'];
+            $code = $fetch_info['code'];
+            if ($status == "verified") {
+                if ($code != 0) {
+                    header('Location: reset-code.php');
+                }
+            } else {
+                header('Location: user-otp.php');
+            }
+        }
+    } else {
+        header('Location: login-user.php');
+    }
+    ?>
+
     <nav class="navbar">
         <a class="navbar-brand" href="/">Anomaly Panel</a>
         <button type="button" class="btn btn-light"><a href="logout-user.php">Logout</a></button>
     </nav>
     <div class="container">
-        <table class="data-table">
-            <tr class="data-table-header">
-                <th>Nombre</th>
-                <th>Email</th>
-                <th>Teléfono</th>
-            </tr>
-            <?php
-            require '../connection.php';
+        <div class="glassmorphism">
+            <table class="data-table">
+                <thead>
+                    <tr class="data-table-header">
+                        <th>Nombre</th>
+                        <th>Email</th>
+                        <th>Teléfono</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    require '../connection.php';
 
-            $sql = "SELECT * FROM registros";
-            $result = $con->query($sql);
+                    $sql = "SELECT * FROM registros";
+                    $result = $con->query($sql);
 
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                    echo "<tr class='data-table-row'>";
-                    echo "<td class='data-table-cell'>" . $row['nombre'] . "</td>";
-                    echo "<td class='data-table-cell'>" . $row['email'] . "</td>";
-                    echo "<td class='data-table-cell'>" . $row['telefono'] . "</td>";
-                    echo "</tr>";
-                }
-            } else {
-                echo "<tr><td class='data-table-empty' colspan='3'>No hay registros disponibles</td></tr>";
-            }
+                    if ($result->num_rows > 0) {
+                        while ($row = $result->fetch_assoc()) {
+                            echo "<tr class='data-table-row'>";
+                            echo "<td class='data-table-cell'>" . $row['nombre'] . "</td>";
+                            echo "<td class='data-table-cell'>" . $row['email'] . "</td>";
+                            echo "<td class='data-table-cell'>" . $row['telefono'] . "</td>";
+                            echo "<td class='data-table-cell'><i class='fas fa-clipboard copy-icon'></i></td>";
+                            echo "</tr>";
+                        }
+                    } else {
+                        echo "<tr><td class='data-table-empty' colspan='4'>No hay registros disponibles</td></tr>";
+                    }
 
-            $con->close();
-            /*
-
-            _ \    _ \ \ \        /  ____|   _ \   ____|  __ \       __ ) \ \   /     _ _|   \  |   ___|   _ \ __ __|  ____| 
-            |   |  |   | \ \  \   /   __|    |   |  __|    |   |      __ \  \   /        |     \ |  |      |   |   |    __|   
-            ___/   |   |  \ \  \ /    |      __ <   |      |   |      |   |    |         |   |\  |  |      |   |   |    |     
-            _|     \___/    \_/\_/    _____| _| \_\ _____| ____/      ____/    _|       ___| _| \_| \____| \___/   _|   _____| 
-                                                                                                            
-            https://incote.click
-            
-            */
-            ?>
-        </table>
+                    $con->close();
+                    ?>
+                </tbody>
+            </table>
+        </div>
     </div>
 
     <footer class="footer">
         <div class="container">
-            <p class="text-center">Powered by <a href="https://incote.click">Incote</a></p>
+            <p>Powered by <a href="https://cwa.mx">CWA</a> & <a href="https://incote.click">Incote</a></p>
         </div>
     </footer>
 
+    <script>
+        function copyTableRow(event) {
+            var tableRow = event.target.parentNode.parentNode;
+            var range = document.createRange();
+            range.selectNode(tableRow);
+            window.getSelection().removeAllRanges();
+            window.getSelection().addRange(range);
+
+            try {
+                var successful = document.execCommand('copy');
+                if (successful) {
+                    var icon = event.target;
+                    icon.classList.remove('fa-clipboard');
+                    icon.classList.add('fa-check');
+
+                    setTimeout(function() {
+                        icon.classList.remove('fa-check');
+                        icon.classList.add('fa-clipboard');
+                    }, 2000);
+                }
+            } catch (err) {
+                console.log('Error copying table row: ', err);
+            }
+
+            window.getSelection().removeAllRanges();
+        }
+
+        var copyIcons = document.querySelectorAll('.copy-icon');
+        copyIcons.forEach(function(icon) {
+            icon.addEventListener('click', copyTableRow);
+        });
+    </script>
 </body>
 
 </html>
